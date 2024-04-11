c5d6100f1e9dd05b504f932e25d998dffb679f25
Abdoullahi-dev <119604973+Abdoullahi-dev@users.noreply.github.com>
2024-04-11 14:20:55 +0300

    put dynamic graph into class, fiddled with appending new items

--- main.py
+++ main.py
@@ -7,67 +7,71 @@ from matplotlib.backend_bases import key_press_handler
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
                                                NavigationToolbar2Tk)
 from matplotlib.figure import Figure
-root = tkinter.Tk()
-root.wm_title("Water Levels Figure")
-fig = Figure(figsize=(12, 4), dpi=100)
-ax = fig.add_subplot()
-sensor = group_9_data_generator.DataGenerator(0.0, 200.0, 200)
-x, y = sensor.plot()
 
-line = ax.plot(x, y, "b")
-ax.fill_between(x, y)
-ax.set_xlabel('Time, in years')
-ax.set_ylabel('Rise in global sea levels, in millimeters')
-ax.set_title('Global rise in sea levels since 1880')
-#
-# ax.xlim(1872 + num*6, 1880 + num*6.75 + 5*5)
-# ax.ylim(y[0] - 1, y[-1])
-canvas = FigureCanvasTkAgg(fig, master=root)
-canvas.draw()
+class DynamicGraph:
+    def __init__(self):
+        self.root = tkinter.Tk()
+        self.root.wm_title("Water Levels Figure")
+        self.fig = Figure(figsize=(12, 4), dpi=100)
+        self.plot = self.fig.add_subplot()
+        self.sensor = group_9_data_generator.DataGenerator(0.0, 200.0, 200)
+        self.x, self.y = self.sensor.plot()
+        self.length = len(self.y)
+        self.i = 1 / self.length
+        self.plot_graph()
+        self.toolbar = NavigationToolbar2Tk(self.canvas, self.root, pack_toolbar=False)
+        self.toolbar.update()
+        self.button_quit = tkinter.Button(master=self.root, text="Quit", command=self.root.destroy)
+        self.done = False
+        self.pack_all()
+    def plot_graph(self):
+        self.plot.plot(self.x, self.y, "b")
+        self.plot.fill_between(self.x, self.y)
+        self.plot.set_xlabel('Time, in years')
+        self.plot.set_ylabel('Rise in global sea levels, in millimeters')
+        self.plot.set_title('Global rise in sea levels since 1880')
+        if (hasattr(self, "canvas")) == False:
+            self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)
+        self.canvas.draw()
+    def update_plot(self):
+        while not self.done:
+            print(self.y)
+            # abs(self.__generator("gaussian", self.i * 0.035)) + self.__generator("uniform") * 5 + i
 
-# pack_toolbar=False will make it easier to use a layout manager later on.
-toolbar = NavigationToolbar2Tk(canvas, root, pack_toolbar=False)
-toolbar.update()
+            # # m is the y-range of the graph
+            # m = (self.xmax - self.xmin + 1 / self.n)
+            # c = self.xmin
+            # # list_x is the returned value (output of the method _generator() when using the parameter("constant"))
+            # list_x = self.__generator("constant")  # returns a list of normalized values between 0 and 1
+            # # print(f"x: {list_x}")
+            # # y = mx + c, the required linear transformation
+            # y = [(m * x) + c for x in list_x]
+            #
+            # # abs(self.__generator("gaussian", self.i * 0.035)) + self.__generator("uniform") * 5 + i
 
-button_quit = tkinter.Button(master=root, text="Quit", command=root.destroy)
+            self.x.append(self.x[-1] + 0.5)
+            self.y.append(self.y[-1] + ((random.gauss(0, (self.y[-1]) * 0.035)) + random.uniform(0.0, 1)))
 
-done = False
+            self.x.pop(0)
+            self.y.pop(0)
+            # line.set
+            self.plot.clear()
+            self.plot_graph()
+            # required to update canvas and attached toolbar!
+            self.canvas.draw()
+            print("drawn")
+            time.sleep(0.1)
 
+    def create_thread(self):
+        thread = threading.Thread(target=self.update_plot, daemon=True)
+        thread.start()
 
-def update_line():
-    while not done:
-        print(y)
-        x.append(x[-1] + 0.5)
-        y.append(y[-1] + (random.gauss(0, y[-1] * 0.035) + random.uniform(0.0, 5)))
+    def pack_all(self):
+        self.button_generate = tkinter.Button(master=self.root, text="Update", command=self.create_thread)
+        self.button_quit.pack(side=tkinter.BOTTOM)
+        self.button_generate.pack(side=tkinter.BOTTOM)
+        self.toolbar.pack(side=tkinter.BOTTOM, fill=tkinter.X)
+        self.canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=True)
 
-        x.pop(0)
-        y.pop(0)
-        # line.set
-        ax.clear()
-        ax.plot(x, y, "b")
-        ax.fill_between(x, y)
-        # required to update canvas and attached toolbar!
-        canvas.draw()
-        print("drawn")
-        time.sleep(0.1)
-
-
-def create_thread():
-    thread = threading.Thread(target=update_line, daemon=True)
-    thread.start()
-
-
-button_generate = tkinter.Button(master=root, text="Update", command=create_thread)
-# slider_update = tkinter.Scale(root, from_=1, to=5, orient=tkinter.HORIZONTAL,
-#                               command=update_frequency, label="Frequency [Hz]")
-
-# Packing order is important. Widgets are processed sequentially and if there
-# is no space left, because the window is too small, they are not displayed.
-# The canvas is rather flexible in its size, so we pack it last which makes
-# sure the UI controls are displayed as long as possible.
-button_quit.pack(side=tkinter.BOTTOM)
-button_generate.pack(side=tkinter.BOTTOM)
-toolbar.pack(side=tkinter.BOTTOM, fill=tkinter.X)
-canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=True)
-
+graph = DynamicGraph()
 tkinter.mainloop()
