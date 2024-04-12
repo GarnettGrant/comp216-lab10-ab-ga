e87e5b09cfd4a760e458b05b907b5acd07c15885
Abdoullahi-dev <119604973+Abdoullahi-dev@users.noreply.github.com>
2024-04-12 16:31:19 +0300

    changed value generation

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class DynamicGraph:
     def __init__(self):
         self.root = tkinter.Tk()
         self.root.wm_title("Water Levels Figure")
-        self.fig = Figure(figsize=(12, 4), dpi=100)
+        self.fig = Figure(figsize=(8, 4), dpi=100)
         self.plot = self.fig.add_subplot()
         self.sensor = group_9_data_generator.DataGenerator(0.0, 200.0, 200)
         self.x, self.y = self.sensor.plot()
@@ -36,30 +36,26 @@ class DynamicGraph:
     def update_plot(self):
         while not self.done:
             print(self.y)
-            # abs(self.__generator("gaussian", self.i * 0.035)) + self.__generator("uniform") * 5 + i
-
-            # # m is the y-range of the graph
-            # m = (self.xmax - self.xmin + 1 / self.n)
-            # c = self.xmin
-            # # list_x is the returned value (output of the method _generator() when using the parameter("constant"))
-            # list_x = self.__generator("constant")  # returns a list of normalized values between 0 and 1
-            # # print(f"x: {list_x}")
-            # # y = mx + c, the required linear transformation
-            # y = [(m * x) + c for x in list_x]
-            #
-            # # abs(self.__generator("gaussian", self.i * 0.035)) + self.__generator("uniform") * 5 + i
 
             self.x.append(self.x[-1] + 1)
-            self.y.append(random.uniform(self.y[-1] -10, self.y[-1] + 10) + random.gauss(0, self.y[-1] * 0.035))
 
+            # the equation of our line is y = Ax^2 + Bx + C, but C and B are 0 so we only need y = ax^2.
+            # we know that we want when X is 135, Y = 200
+            # therefore:
+            # 200 = A(pow(135,2))
+            # 200/pow(135,2) = A
+            # 200/pow(135,2) = 0.01097393689986282578875171467764
+            # now that we have A, we can multiply it by any X value squared to get the accurate y position
+            new_y = 0.01097393689986282578875171467764 * pow(self.x[-1] - 1880, 2) # because we start at 1880, we subtract 1880 from x
+            #add random noise to this new y position and then append to the list
+            self.y.append( new_y + (random.gauss(0, new_y/(self.x[-1] - 1880) * 2)) + random.uniform(0, 5))
             self.x.pop(0)
             self.y.pop(0)
             # line.set
             self.plot.clear()
             self.plot_graph()
-            # required to update canvas and attached toolbar!
+            # redraw graph
             self.canvas.draw()
-            print("drawn")
             time.sleep(0.1)
 
     def create_thread(self):
